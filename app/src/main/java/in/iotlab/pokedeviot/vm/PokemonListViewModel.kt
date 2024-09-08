0aba47318a2ea385e5f70b1a2cc94ab2e043740e
Abhranil Dasgupta <abhranilnxt@gmail.com>
2024-09-08 14:18:21 +0530

    feat - refactoring

--- app/src/main/java/in/iotlab/pokedeviot/vm/PokemonListViewModel.kt
+++ app/src/main/java/in/iotlab/pokedeviot/vm/PokemonListViewModel.kt
@@ -1,19 +1,13 @@
 package `in`.iotlab.pokedeviot.vm
 
-import android.graphics.Bitmap
-import android.graphics.drawable.BitmapDrawable
-import android.graphics.drawable.Drawable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.palette.graphics.Palette
 import dagger.hilt.android.lifecycle.HiltViewModel
 import `in`.iotlab.pokedeviot.data.model.PokedexListEntry
 import `in`.iotlab.pokedeviot.data.repo.PokemonRepository
 import `in`.iotlab.pokedeviot.data.utils.Constants.PAGE_SIZE
 import `in`.iotlab.pokedeviot.data.utils.UiState
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.util.Locale
 import javax.inject.Inject
@@ -29,39 +23,10 @@ class PokemonListViewModel @Inject constructor(
     var loadError = mutableStateOf("")
     var isLoading = mutableStateOf(false)
     var endReached = mutableStateOf(false)
-
-    private var cachedPokemonList = listOf<PokedexListEntry>()
-    private var isSearchStarting = true
     var isSearching = mutableStateOf(false)
 
     init {
         loadPokemonPaginated()
-    }
-
-    fun searchPokemonList(query: String) {
-        val listToSearch = if(isSearchStarting) {
-            pokemonList.value
-        } else {
-            cachedPokemonList
-        }
-        viewModelScope.launch(Dispatchers.Default) {
-            if(query.isEmpty()) {
-                pokemonList.value = cachedPokemonList
-                isSearching.value = false
-                isSearchStarting = true
-                return@launch
-            }
-            val results = listToSearch.filter {
-                it.pokemonName.contains(query.trim(), ignoreCase = true) ||
-                        it.number.toString() == query.trim()
-            }
-            if(isSearchStarting) {
-                cachedPokemonList = pokemonList.value
-                isSearchStarting = false
-            }
-            pokemonList.value = results
-            isSearching.value = true
-        }
     }
 
     fun loadPokemonPaginated() {
